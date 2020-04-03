b1871ed2f536fa9a2be16c02e9b483891f7676d2
Martin-PC <Martin-PC@192.168.0.119>
2020-04-03 17:07:32 +0300

    First Commit

---
+++ DesignPatterns/src/FactoryMain.java
@@ -0,0 +1,15 @@
+import com.phone.Android;
+import com.phone.OS;
+import com.phone.OperatingSystemFactory;
+import com.phone.Windows;
+
+public class FactoryMain 
+{ 
+	public static void main (String a [])
+	{
+		OperatingSystemFactory osf = new OperatingSystemFactory();
+		OS obj = osf.getInstance("Csed");
+		obj.spec();
+	}
+
+}
